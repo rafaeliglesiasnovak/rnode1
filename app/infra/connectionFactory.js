3a690211293400d39ca6ce912c6545c13a72fcde
Rafael Novak <rafaeliglesiasnovak@gmail.com>
2017-11-23 22:01:10 -0200

    mudancas

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -2,20 +2,24 @@ var mysql  = require('mysql');
 
 function createDBConnection() {
     if(!process.env.NODE_ENV) {
+        var urlConexion = process.env.CLEARDB_DATABASE_URL;
+        var groups = urlConexion.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
         return mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'tcc'
+            host: groups[2],
+            user: groups[0],
+            password: groups[1],
+            database: groups[3]
         });
     }
 
     if(process.env.NODE_ENV == 'test') {
+        var urlConexion = process.env.CLEARDB_DATABASE_URL;
+        var groups = urlConexion.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
         return mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'tcc'
+            host: groups[2],
+            user: groups[0],
+            password: groups[1],
+            database: groups[3]
         });
     }
 }
