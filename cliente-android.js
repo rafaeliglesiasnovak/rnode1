38e18f7ef0076fb1e45b5a24d52c22753f796da5
Rafael Novak <rafaeliglesiasnovak@gmail.com>
2017-11-23 22:21:21 -0200

    mudancas 4

--- cliente-android.js
+++ cliente-android.js
@@ -1,8 +1,18 @@
 var http = require('http');
 
+// var configuracoes = {
+//     hostname: 'localhost',
+//     port: 3000,
+//     path: '/celulares/json',
+//     headers: {
+//         'Accept': 'application/json'
+//     }
+// };
+
 var configuracoes = {
-    hostname: 'localhost',
-    port: 3000,
+    var port = process.env.PORT || 3000;
+    hostname: 'us-cdbr-iron-east-05.cleardb.net',
+    port: port,
     path: '/celulares/json',
     headers: {
         'Accept': 'application/json'
